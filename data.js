42bbd34079751b9b51396e1c7bcd0c98c1132fb7
mohamad naji <you@example.com>
2024-08-04 07:29:37 +0300

    version 1

---
+++ data.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+
+const PORT = process.env.PORT || 3030;
+
+
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+var teams=[{
+    id:1,
+    name:"mohamad naji mohamad",
+    adress:"aydamoun akkar",
+    position:"team leader",
+    salary:"1500$"
+},{
+id:2,
+name:"khoder alchami",
+adress:"sayda",
+position:"waiter",
+salary:"1000$"
+},{  id:3,
+    name:"youssef kasba",
+    adress:"koura",
+    position:"manager",
+    salary:"2000$"
+},{
+    id:4,
+    name:"moustafa oumeis ",
+    adress:"karoon",
+    position:"opperation manager",
+    salary:"5000$"
+
+}]
+
+app.get('/', function (req, res) {
+    res.send(teams)
+  })
